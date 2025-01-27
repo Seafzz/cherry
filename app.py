2ffeaf9d583290e9afd7b20977fa40c43aa4a236
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2025-01-27 09:16:31 +0000

    added page scripts and paths

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ from app_pages.multipage import MultiPage
 
 #load page scripts
 from app_pages.page_summary import page_summary_body
+from app_pages.page_hypothesis import hypothesis_page
+from app_pages.page_data_visualizer import visual_page
+from app_pages.page_data_prediction import prediction_page
+from app_pages.page_performance import performance_page
