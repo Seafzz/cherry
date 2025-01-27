43a4dd919f9fff245d202aa0d9fe880da1795f8b
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2025-01-27 09:20:08 +0000

    added pages to the app

--- app.py
+++ app.py
@@ -7,3 +7,12 @@ from app_pages.page_hypothesis import hypothesis_page
 from app_pages.page_data_visualizer import visual_page
 from app_pages.page_data_prediction import prediction_page
 from app_pages.page_performance import performance_page
+
+app = MultiPage(app_name='cherry-leaves')
+
+# Add pages to the app
+app.add_page("Summary", page_summary_body)
+app.add_page("Hypothesis", hypothesis_page)
+app.add_page("Data Visualizer", visual_page)
+app.add_page("Data Prediction", prediction_page)
+app.add_page("Performance", performance_page)
