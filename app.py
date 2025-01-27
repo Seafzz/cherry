bdae3477ae22fa731ada49a39ca1eb77e86deb99
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2025-01-27 09:41:17 +0000

    fixed some smaller typos in the comments and pages

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 import streamlit as st
 from app_pages.multipage import MultiPage
 
-#load page scripts
+# Load page scripts
 from app_pages.page_summary import page_summary_body
 from app_pages.page_hypothesis import hypothesis_page
 from app_pages.page_data_visualizer import visual_page
 from app_pages.page_data_prediction import prediction_page
 from app_pages.page_performance import performance_page
 
-app = MultiPage(app_name='cherry-leaves')
+# Initialize the app
+app = MultiPage(app_name='Cherry Leaves')
 
 # Add pages to the app
 app.add_page("Summary", page_summary_body)
 app.add_page("Hypothesis", hypothesis_page)
 app.add_page("Data Visualizer", visual_page)
-app.add_page("Data Prediction", prediction_page)
+app.add_page("Prediction", prediction_page)
 app.add_page("Performance", performance_page)
 
+# Run the app
 app.run()
+
